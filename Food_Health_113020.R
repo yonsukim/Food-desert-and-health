fef21e457eedf85d26db9abe259ede4663367b11
Yonsu <yonsuk@uci.edu>
2020-12-02 16:36:17 -0800

    Two tables merged

--- Food_Health_113020.R
+++ Food_Health_113020.R
@@ -3,14 +3,62 @@
 #####  YKim - 11/30/20
 
 install.packages("readstata13")
+install.packages("rlang", dependencies = TRUE)
+install.packages("readxl")
+
+install.packages("tidyverse")
+
 library(readstata13)
+library("readxl")
+# library(readxl)
+library(dplyr)
 
-library(readxl)
+# See below if excel not loaded 
+# https://stackoverflow.com/questions/53542831/how-to-install-specific-version-of-rlang-package-in-r
 
-data <- read.dta13("C:\\Users\\Dongwoo\\Documents\\R\\Data\\socal_blocks_someemps_2010b_2010.dta")
-View(data)
+df1 <- read.dta13("C:\\Users\\Dongwoo\\Documents\\R\\Data\\socal_blocks_someemps_2010b_2010.dta")
+# View(df1)
 
 
+df2 <- read_excel("C:\\Users\\Dongwoo\\Documents\\R\\Data\\LA_2.xlsx", sheet="Sheet1")
+# View(df2)
+
+
+# remove.packages(rlang)
+# sessionInfo()
+
+
+df10$tract0  <- substr(df1$blkidfp10, 2, 11)
+
+df20 <- df2 %>%
+  mutate(tract0=as.character(Geo_FIPS))
+
+# View(df10)
+# View(df20)
+
+comb <- df20 %>%
+  dplyr::left_join(df10, by="tract0")
+
+# View(comb) - Two tables merged
+
+# 12/2/2020
+
+
+
+head(df22)
+
+frame <- copy_to(sc, df2, overwrite = TRUE)
+
+View(df1)
+
+df3 <- merge(df2, data df1, by=”ID”)
+
+
+
+
+
+
+c
 install.packages("xlsx")
 library("xlsx")
 
@@ -25,6 +73,11 @@ write.xlsx(data, "C:\\Users\\Dongwoo\\Dropbox\\Research\\Food_health\\Food_store
 
 C:\Users\Dongwoo\Dropbox\YK_JW\Data
 
+install.packages("readxl")
+install.packages("scales")
+install.packages("rlang")
+# install.packages("tidyverse")
+# install.packages("readr")
 
 
 
@@ -32,6 +85,7 @@ C:\Users\Dongwoo\Dropbox\YK_JW\Data
 install.packages("readstata13")
 library(readstata13)
 library(foreign)
+library(rlang)
 
 
 
